f6909fa5fb51ae8b6056887990773b7ed84a6a22
Jheff4 <ogbevoenetinosa@gmail.com>
2026-04-13 19:46:19 +0100

    updated conversations endpoint

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,11 +1,9 @@
 import { mutation, query } from "../_generated/server";
 import { components, internal } from "../_generated/api";
 import { ConvexError, v } from "convex/values";
-// import { supportAgent } from "../system/ai/agents/supportAgent";
-// import { MessageDoc, saveMessage } from "@convex-dev/agent";
-import { paginationOptsValidator } from "convex/server";
 import { supportAgent } from "../system/ai/agents/supportAgent";
-import { saveMessage } from "@convex-dev/agent";
+import { MessageDoc, saveMessage } from "@convex-dev/agent";
+import { paginationOptsValidator } from "convex/server";
 
 export const getMany = query({
   args: {
@@ -30,33 +28,33 @@ export const getMany = query({
       .order("desc")
       .paginate(args.paginationOpts);
 
-    // const conversationsWithLastMessage = await Promise.all(
-    //   conversations.page.map(async (conversation) => {
-    //     let lastMessage: MessageDoc | null = null;
+    const conversationsWithLastMessage = await Promise.all(
+      conversations.page.map(async (conversation) => {
+        let lastMessage: MessageDoc | null = null;
 
-    //     const messages = await supportAgent.listMessages(ctx, {
-    //       threadId: conversation.threadId,
-    //       paginationOpts: { numItems: 1, cursor: null },
-    //     });
+        const messages = await supportAgent.listMessages(ctx, {
+          threadId: conversation.threadId,
+          paginationOpts: { numItems: 1, cursor: null },
+        });
 
-    //     if (messages.page.length > 0) {
-    //       lastMessage = messages.page[0] ?? null;
-    //     }
+        if (messages.page.length > 0) {
+          lastMessage = messages.page[0] ?? null;
+        }
 
-    //     return {
-    //       _id: conversation._id,
-    //       _creationTime: conversation._creationTime,
-    //       status: conversation.status,
-    //       organizationId: conversation.organizationId,
-    //       threadId: conversation.threadId,
-    //       lastMessage,
-    //     };
-    //   })
-    // );
+        return {
+          _id: conversation._id,
+          _creationTime: conversation._creationTime,
+          status: conversation.status,
+          organizationId: conversation.organizationId,
+          threadId: conversation.threadId,
+          lastMessage,
+        };
+      })
+    );
 
     return {
       ...conversations,
-      // page: conversationsWithLastMessage,
+      page: conversationsWithLastMessage,
     };
   },
 });
@@ -120,12 +118,12 @@ export const create = mutation({
       contactSessionId: args.contactSessionId,
     });
 
-    // const widgetSettings = await ctx.db
-    //   .query("widgetSettings")
-    //   .withIndex("by_organization_id", (q) => 
-    //     q.eq("organizationId", args.organizationId),
-    //   )
-    //   .unique();
+    const widgetSettings = await ctx.db
+      .query("widgetSettings")
+      .withIndex("by_organization_id", (q) => 
+        q.eq("organizationId", args.organizationId),
+      )
+      .unique();
 
     const { threadId } = await supportAgent.createThread(ctx, {
       userId: args.organizationId,
