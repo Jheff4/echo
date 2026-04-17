3227c6613f422e17cdca544854da88aab92ec8de
Jheff4 <ogbevoenetinosa@gmail.com>
2026-04-17 16:05:42 +0100

    built the dashboard inbox component

--- apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
@@ -3,18 +3,18 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@workspace/ui/components/resizable";
-// import { ConversationsPanel } from "../components/conversations-panel";
+import { ConversationsPanel } from "../components/conversations-panel";
 
 export const ConversationsLayout = ({
   children
 }: { children: React.ReactNode; }) => {
   return (
     <ResizablePanelGroup className="h-full flex-1" direction="horizontal">
-      <ResizablePanel defaultSize={30} maxSize={30} minSize={20}>
-        <div>Conversations</div>
+      <ResizablePanel defaultSize={20} maxSize={400} minSize={50}>
+        <ConversationsPanel />
       </ResizablePanel>
       <ResizableHandle />
-      <ResizablePanel className="h-full" defaultSize={70}>
+      <ResizablePanel className="h-full" defaultSize={80}>
         {children}
       </ResizablePanel>
     </ResizablePanelGroup>
