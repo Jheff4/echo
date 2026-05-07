0159fe172fa69cf8cfb8ec04e66bf776c3b3ea6b
Jheff4 <ogbevoenetinosa@gmail.com>
2026-05-07 23:52:58 +0100

    updated widget settings form schema

--- apps/web/modules/customization/schemas.ts
+++ apps/web/modules/customization/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const widgetSettingsSchema = z.object({
-  greetMessage: z.string().min(1, "Greeting message is required"),
+  greetMessage: z.string().min(1, "Greeting message is required!"),
   defaultSuggestions: z.object({
     suggestion1: z.string().optional(),
     suggestion2: z.string().optional(),
